8131f255aee56bfcfeb582332ff9c5a7bc7b7bee
Emanuel Silva <manusilvaribeiro10@gmail.com>
2024-10-28 09:54:44 -0300

    Added movie player

--- hooks/useMovie.ts
+++ hooks/useMovie.ts
@@ -1,9 +1,10 @@
 import useSWR from "swr";
 import fetcher from "@/lib/fetcher";
+import { Movie } from "@prisma/client";
 
 const useMovie = (id?: string) => {
-  const { data, error, isLoading } = useSWR(
-    id ? `/api/movie/${id}` : null,
+  const { data, error, isLoading } = useSWR<Movie>(
+    id ? `/api/movies/${id}` : null,
     fetcher,
     {
       revalidateIfStale: false,
